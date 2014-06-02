e48f3350c34b50fdf70b30e1c5d959aa3b245e69
Elliot Winkler <elliot.winkler@gmail.com>
2014-06-01 18:00:05 -0600

    Rename obscure variables

--- lib/simplex.rb
+++ lib/simplex.rb
@@ -12,28 +12,28 @@ class Simplex
 
   attr_accessor :max_pivots
 
-  def initialize(c, a, b)
+  def initialize(coefficients, constraints, rhs_values)
     @pivot_count = 0
     @max_pivots = DEFAULT_MAX_PIVOTS
 
     # Problem dimensions
-    @num_non_slack_vars = a.first.length
-    @num_constraints    = b.length
+    @num_non_slack_vars = constraints.first.length
+    @num_constraints    = rhs_values.length
     @num_vars           = @num_non_slack_vars + @num_constraints
 
-    # Set up initial matrix A and vectors b, c
-    @c = Vector[*c.map {|c1| -1*c1 } + [0]*@num_constraints]
-    @a = a.map {|a1| Vector[*(a1.clone + [0]*@num_constraints)]}
-    @b = Vector[*b.clone]
+    # Set up initial matrix A and vectors rhs_values, coefficients
+    @objective_coefficients = Vector[*coefficients.map {|coefficient| -1*coefficient } + [0]*@num_constraints]
+    @constraints = constraints.map {|constraint| Vector[*(constraint.clone + [0]*@num_constraints)]}
+    @rhs_values = Vector[*rhs_values.clone]
 
-    unless @a.all? {|a| a.size == @c.size } and @b.size == @a.length
+    unless @constraints.all? {|constraints| constraints.size == @objective_coefficients.size } and @rhs_values.size == @constraints.length
       raise ArgumentError, "Input arrays have mismatched dimensions" 
     end
 
-    0.upto(@num_constraints - 1) {|i| @a[i][@num_non_slack_vars + i] = 1 }
+    0.upto(@num_constraints - 1) {|i| @constraints[i][@num_non_slack_vars + i] = 1 }
 
     # set initial solution: all non-slack variables = 0
-    @x          = Vector[*([0]*@num_vars)]
+    @solution          = Vector[*([0]*@num_vars)]
     @basic_vars = (@num_non_slack_vars...@num_vars).to_a
     update_solution
   end
@@ -44,17 +44,17 @@ class Simplex
   end
 
   def current_solution
-    @x.to_a[0...@num_non_slack_vars]
+    @solution.to_a[0...@num_non_slack_vars]
   end
 
   def update_solution
-    0.upto(@num_vars - 1) {|i| @x[i] = 0 }
+    0.upto(@num_vars - 1) {|i| @solution[i] = 0 }
 
     @basic_vars.each do |basic_var|
       row_with_1 = row_indices.detect do |row_ix|
-        @a[row_ix][basic_var] == 1
+        @constraints[row_ix][basic_var] == 1
       end
-      @x[basic_var] = @b[row_with_1]
+      @solution[basic_var] = @rhs_values[row_with_1]
     end
   end
 
@@ -71,12 +71,12 @@ class Simplex
   end
 
   def variables
-    (0...@c.size).to_a
+    (0...@objective_coefficients.size).to_a
   end
 
   def entering_variable
-    variables.select { |var| @c[var] < 0 }.
-              min_by { |var| @c[var] }
+    variables.select { |var| @objective_coefficients[var] < 0 }.
+              min_by { |var| @objective_coefficients[var] }
   end
 
   def pivot
@@ -86,20 +86,20 @@ class Simplex
     leaving_var  = basic_variable_in_row(pivot_row)
     replace_basic_variable(leaving_var => pivot_column)
 
-    pivot_ratio = Rational(1, @a[pivot_row][pivot_column])
+    pivot_ratio = Rational(1, @constraints[pivot_row][pivot_column])
 
     # update pivot row
-    @a[pivot_row] *= pivot_ratio
-    @b[pivot_row] = pivot_ratio * @b[pivot_row]
+    @constraints[pivot_row] *= pivot_ratio
+    @rhs_values[pivot_row] = pivot_ratio * @rhs_values[pivot_row]
 
     # update objective
-    @c -= @c[pivot_column] * @a[pivot_row]
+    @objective_coefficients -= @objective_coefficients[pivot_column] * @constraints[pivot_row]
 
     # update A and B
     (row_indices - [pivot_row]).each do |row_ix|
-      r = @a[row_ix][pivot_column]
-      @a[row_ix] -= r * @a[pivot_row]
-      @b[row_ix] -= r * @b[pivot_row]
+      r = @constraints[row_ix][pivot_column]
+      @constraints[row_ix] -= r * @constraints[pivot_row]
+      @rhs_values[row_ix] -= r * @rhs_values[pivot_row]
     end
 
     update_solution
@@ -114,30 +114,30 @@ class Simplex
 
   def pivot_row(column_ix)
     row_ix_a_and_b = row_indices.map { |row_ix|
-      [row_ix, @a[row_ix][column_ix], @b[row_ix]]
-    }.reject { |_, a, b|
-      a == 0
-    }.reject { |_, a, b|
-      (b < 0) ^ (a < 0) # negative sign check
+      [row_ix, @constraints[row_ix][column_ix], @rhs_values[row_ix]]
+    }.reject { |_, constraints, rhs_values|
+      constraints == 0
+    }.reject { |_, constraints, rhs_values|
+      (rhs_values < 0) ^ (constraints < 0) # negative sign check
     }
-    row_ix, _, _ = *last_min_by(row_ix_a_and_b) { |_, a, b|
-      Rational(b, a)
+    row_ix, _, _ = *last_min_by(row_ix_a_and_b) { |_, constraints, rhs_values|
+      Rational(rhs_values, constraints)
     }
     row_ix
   end
 
   def basic_variable_in_row(pivot_row)
     column_indices.detect do |column_ix|
-      @a[pivot_row][column_ix] == 1 and @basic_vars.include?(column_ix)
+      @constraints[pivot_row][column_ix] == 1 and @basic_vars.include?(column_ix)
     end
   end
 
   def row_indices
-    (0...@a.length).to_a
+    (0...@constraints.length).to_a
   end
 
   def column_indices
-    (0...@a.first.size).to_a
+    (0...@constraints.first.size).to_a
   end
 
   def formatted_tableau
@@ -147,28 +147,28 @@ class Simplex
     else
       pivot_row = nil
     end
-    num_cols = @c.size + 1
-    c = formatted_values(@c.to_a)
-    b = formatted_values(@b.to_a)
-    a = @a.to_a.map {|ar| formatted_values(ar.to_a) }
+    num_cols = @objective_coefficients.size + 1
+    coefficients = formatted_values(@objective_coefficients.to_a)
+    rhs_values = formatted_values(@rhs_values.to_a)
+    constraints = @constraints.to_a.map {|ar| formatted_values(ar.to_a) }
     if pivot_row
-      a[pivot_row][pivot_column] = "*" + a[pivot_row][pivot_column]
+      constraints[pivot_row][pivot_column] = "*" + constraints[pivot_row][pivot_column]
     end
-    max = (c + b + a + ["1234567"]).flatten.map(&:size).max
+    max = (coefficients + rhs_values + constraints + ["1234567"]).flatten.map(&:size).max
     result = []
-    result << c.map {|c| c.rjust(max, " ") }
-    a.zip(b) do |arow, brow|
-      result << (arow + [brow]).map {|a| a.rjust(max, " ") }
-      result.last.insert(arow.length, "|")
+    result << coefficients.map {|coefficients| coefficients.rjust(max, " ") }
+    constraints.zip(rhs_values) do |constraint_row, rhs_value|
+      result << (constraint_row + [rhs_value]).map {|constraints| constraints.rjust(max, " ") }
+      result.last.insert(constraint_row.length, "|")
     end
-    lines = result.map {|b| b.join("  ") }
+    lines = result.map {|rhs_values| rhs_values.join("  ") }
     max_line_length = lines.map(&:length).max
     lines.insert(1, "-"*max_line_length)
     lines.join("\n")
   end
 
   def formatted_values(array)
-    array.map {|c| "%2.3f" % c }
+    array.map {|coefficients| "%2.3f" % coefficients }
   end
 
   # like Enumerable#min_by except if multiple values are minimum 
